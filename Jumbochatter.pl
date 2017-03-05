3dc106f69c7a6933360239a055f03734711b9cda
Kristian Nielsen <knielsen@knielsen-hq.org>
2017-03-05 13:22:18 +0100

    Also answer on privmsg.

--- Jumbochatter.pl
+++ Jumbochatter.pl
@@ -21,33 +21,36 @@ sub public_hook {
     if ($target =~ m/#(?:labitat|(?:kn)?test)/i &&
         $msg =~ m/jumbotron/i &&
         $msg =~ m/power|str..?m/i) {
-        blipreq_start($server, $target, $msg);
+        blipreq_start($server, lc($target), $msg);
     }
 }
 
 sub private_hook {
     my ($server, $msg, $nick, $nick_addr) = @_;
+    return if $nick =~ /^#/;
+    if ($msg =~ m/power|str..?m/i) {
+        blipreq_start($server, $nick, $msg);
+    }
 }
 
 
 sub blipreq_start {
   my ($server, $target, $msg) = @_;
-  my $lc_target = lc($target);
 
   if ($msg =~ /stram/i) {
-    $server->command("MSG $lc_target http://www.stram.cz/");
+    $server->command("MSG $target http://www.stram.cz/");
     return;
   }
   if ($msg =~ /begejstr/i) {
-    $server->command("MSG $lc_target http://www.bs.dk/publikationer/andre/faglig/images/s26.jpg");
+    $server->command("MSG $target http://www.bs.dk/publikationer/andre/faglig/images/s26.jpg");
     return;
   }
   if ($msg =~ /fr[iou]str[iou]m/i) {
-    $server->command("MSG $lc_target http://www.jf-koeleteknik.dk/produkter/koele-og-frostanlaeg/");
+    $server->command("MSG $target http://www.jf-koeleteknik.dk/produkter/koele-og-frostanlaeg/");
     return;
   }
   unless ($msg =~ /power/i || $msg =~ /str(?:\x{c3}\x{b8}|\x{f8})m/i) {
-    $server->command("MSG $lc_target https://www.youtube.com/watch?v=Ka8bIPqKxiA");
+    $server->command("MSG $target https://www.youtube.com/watch?v=Ka8bIPqKxiA");
     return;
   }
 
@@ -66,7 +69,7 @@ sub blipreq_start {
     my $usage_15min = 3600000*$count / $sum;
     my $text = sprintf("Power usage: %.1f W. 15 minute average: %.1f W",
                        $usage_now, $usage_15min);
-    $server->command("MSG $lc_target $text");
+    $server->command("MSG $target $text");
   });
   # A bit tricky here, http_get() return is very magic. Unless called in a void
   # context, a "cancellation guard" is returned, which if destroyed will cancel
